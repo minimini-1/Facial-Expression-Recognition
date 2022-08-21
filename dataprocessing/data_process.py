a49dad882b25b513a9bd088339bd65eba9e1a002
jongmin4422 <jongmin4422@naver.com>
2022-08-22 00:36:32 +0900

    change code

--- dataprocessing/data_process.py
+++ dataprocessing/data_process.py
@@ -5,7 +5,7 @@ import os
 import PIL
 import PIL.Image
 import tensorflow as tf
-import tensorflow_datasets as tfds
+# import tensorflow_datasets as tfds
 import IPython.display as display
 from tqdm import tqdm
 import random
