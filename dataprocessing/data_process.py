002b4659ee218accc26bece84e3cac9e9fed8c15
jongmin4422 <jongmin4422@naver.com>
2022-09-07 23:49:20 +0900

    change tfrecord code

--- dataprocessing/data_process.py
+++ dataprocessing/data_process.py
@@ -10,39 +10,39 @@ import IPython.display as display
 from tqdm import tqdm
 import random
 import sys
+import glob
 
-def data_to_tfrecord(imagepath, outputpath):
+def data_to_tfrecord(imagepath, outputpath, mode):
     # 여러 개의 폴더에 있는 것 합치기
     images, label_list = [], []
     path = imagepath
     for folder in os.listdir(path):
         i = 0       
-        landmark_df = pd.read_csv(path+folder+"/"+folder.split("+")[0]+".csv")
+        landmark_df = pd.read_csv(glob.glob(path+folder+"/*.csv")[0])        
         landmark_columns = [point for point in landmark_df.columns if "-" in point] 
         for image in tqdm(os.listdir(path+folder), desc=folder+" 폴더 작업중"):     
-            if '+0' in folder:
-                if '.csv' in image: #landmark point
-                    continue 
-                if np.nan in landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))]:
-                    continue
-                images.append({'path': path+folder+"/"+image, 'class': 0, 'landmark_points': landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))][landmark_columns].to_numpy()[0]})
-                label_list.append(0)
-            elif '+5' in folder:
-                if '.csv' in image: #landmark point                    
-                    continue
-                if np.nan in landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))]:
-                    continue
-                images.append({'path': path+folder+"/"+image, 'class': 0.5, 'landmark_points': landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))][landmark_columns].to_numpy()[0]})
-                label_list.append(0.5)
-            elif '+1' in folder:
-                if '.csv' in image: #landmark point
-                    # landmark_df = pd.read_csv(image)
-                    # landmark_columns = [point for point in landmark_df.columns if "-" in point]
-                    continue
-                if np.nan in landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))]:
-                    continue
-                images.append({'path': path+folder+"/"+image, 'class': 1, 'landmark_points': landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))][landmark_columns].to_numpy()[0]})
-                label_list.append(1)
+            # if '+0' in folder:
+            if '.csv' in image: #landmark point
+                continue 
+            if np.nan in landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))]:
+                continue
+            images.append({'path': path+folder+"/"+image, 'class': int(folder.split('+')[1]), 'landmark_points': landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))][landmark_columns].to_numpy()[0]})
+            # elif '+5' in folder:
+            #     if '.csv' in image: #landmark point                    
+            #         continue
+            #     if np.nan in landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))]:
+            #         continue
+            #     images.append({'path': path+folder+"/"+image, 'class': 0.5, 'landmark_points': landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))][landmark_columns].to_numpy()[0]})
+            #     label_list.append(0.5)
+            # elif '+1' in folder:
+            #     if '.csv' in image: #landmark point
+            #         # landmark_df = pd.read_csv(image)
+            #         # landmark_columns = [point for point in landmark_df.columns if "-" in point]
+            #         continue
+            #     if np.nan in landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))]:
+            #         continue
+            #     images.append({'path': path+folder+"/"+image, 'class': 1, 'landmark_points': landmark_df[landmark_df['frame'] == float(image.replace(".jpg", ""))][landmark_columns].to_numpy()[0]})
+            #     label_list.append(1)
             i += 1
     # print('num_classes: ', len(set(label_list)))
     # encoder = class_one_hot_encoder(label_list)
@@ -53,10 +53,14 @@ def data_to_tfrecord(imagepath, outputpath):
     random.shuffle(images)
     with tf.io.TFRecordWriter(save_path) as f:
         for image in tqdm(images, desc='이미지를 tfrecord에 저장중'):
-
-            image_class = image['class']
-            # print(image['landmark_points'])
+            label = [0] * 7
             image_landmark = image['landmark_points']
+            if mode == 'multi':
+                label[image['class']] = 1
+                image_class = label
+            else:
+                image_class = image['class']
+            # print(image['landmark_points'])
             # image_class = float(image_class)
 
             try:
@@ -85,16 +89,28 @@ def data_to_tfrecord(imagepath, outputpath):
                 # landmark_list = point_adjust(landmark_list, image_width, image_height)
 
                 # record
-                record = tf.train.Example(
-                    features=tf.train.Features(
-                        feature={
-                            'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_file])),
-                            'class': tf.train.Feature(int64_list=tf.train.Int64List(value=[image_class])),
-                            'landmark_points': tf.train.Feature(float_list=tf.train.FloatList(value=image_landmark))
-                            # 'bb_box': tf.train.Feature(float_list=tf.train.FloatList(value=bb_box_list)),
-                        }
+                if mode == 'multi':
+                    record = tf.train.Example(
+                        features=tf.train.Features(
+                            feature={
+                                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_file])),
+                                'class': tf.train.Feature(int64_list=tf.train.Int64List(value=image_class)),
+                                'landmark_points': tf.train.Feature(float_list=tf.train.FloatList(value=image_landmark))
+                                # 'bb_box': tf.train.Feature(float_list=tf.train.FloatList(value=bb_box_list)),
+                            }
+                        )
                     )
-                )
+                else:
+                    record = tf.train.Example(
+                        features=tf.train.Features(
+                            feature={
+                                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_file])),
+                                'class': tf.train.Feature(int64_list=tf.train.Int64List(value=[image_class])),
+                                'landmark_points': tf.train.Feature(float_list=tf.train.FloatList(value=image_landmark))
+                                # 'bb_box': tf.train.Feature(float_list=tf.train.FloatList(value=bb_box_list)),
+                            }
+                        )
+                    )
                 f.write(record.SerializeToString())
                 i += 1
             except Exception as e:
@@ -102,4 +118,4 @@ def data_to_tfrecord(imagepath, outputpath):
                 continue
 
     print('이미지 갯수: ', i)
-    print(label_list)
+    print([i['class'] for i in images])
